54d8325e509a8e7295c0c3eee6b17a0beb175b4f
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-06-15 19:04:03 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,11 +2,11 @@ def prime?(num)
   counter = 2
   if num.abs <2
       return false
-  elsif num.abs % counter == 0 && num.abs!=counter
+    end
+  if num.abs % counter == 0 && num.abs!=counter
       return false
+    end
+    counter += 1
   end
-counter += 1
-end
-return true
-end
+  return true
 end
