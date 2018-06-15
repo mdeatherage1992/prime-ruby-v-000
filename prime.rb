cc82479d118ba60871a4b2a2c36d5daa16223941
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-06-15 18:59:33 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,14 +1,10 @@
 def prime?(number)
-  if number <= 2
+i = nil
+while i < number
+  if number % i == 0
+    return false
+  else
     return true
   end
- i = 2
- while i < number
-   if number % i == 0
-     return false
-   else
-     return true
-   end
-   i += 1
- end
- end
+end
+end
