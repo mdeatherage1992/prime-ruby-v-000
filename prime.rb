822ebc6a6f20552426b7bea0c56b7ff1cfbe90fb
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-06-15 19:06:46 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,13 +1,13 @@
 def prime?(num)
-  counter = 2
-  if num.abs <2
-      return false
-    end
-  if num.abs % counter == 0 && num.abs!=counter
-      return false
-    end
-    counter += 1
-  end
-  return true
-end
-end
+ if num.abs < 2
+   return true
+ end
+ i = 2
+ while i < num
+   if num % i == 0
+     return false
+   end
+   i += 1
+ end
+ return true
+ end
